81f69f912fb9e48146557d4498f139c728961019
angelodusba <angelodusba@gmail.com>
2024-12-28 17:16:26 +0100

    segment first implementation

--- home/os161/src/kern/include/segment.h
+++ home/os161/src/kern/include/segment.h
@@ -1,1 +1,42 @@
-// TODO:
+#ifndef _SEGMENT_H_
+#define _SEGMENT_H_
+
+#include <types.h>
+
+/**
+ * @struct Segment
+ * @brief Represents a segment in a program's address space.
+ * 
+ * This structure stores information about an ELF file segment,
+ * including its offset in the ELF file, starting virtual address,
+ * and the number of pages it occupies.
+ */
+struct segment {
+    off_t elf_offset;
+    vaddr_t start_vaddr;
+    size_t num_pages;
+};
+
+/**
+ * @brief Creates a new segment structure and initializes it to default values.
+ * 
+ * Allocates memory for a `struct segment` and sets its fields to default values.
+ * 
+ * @return A pointer to the newly created segment.
+ */
+struct segment *segment_create(void);
+
+/**
+ * @brief Defines the properties of an existing segment.
+ * 
+ * Initializes a segment with specific values for ELF offset, starting virtual
+ * address, and the number of pages.
+ * 
+ * @param seg A pointer to the segment structure to be initialized.
+ * @param elf_offset The ELF offset for the segment.
+ * @param start_vaddr The starting virtual address of the segment.
+ * @param num_pages The number of pages in the segment.
+ */
+void segment_define(struct segment *seg, off_t elf_offset, vaddr_t start_vaddr, size_t num_pages); 
+
+#endif 
--- home/os161/src/kern/vm/segment.c
+++ home/os161/src/kern/vm/segment.c
@@ -1,1 +1,15 @@
-// TODO:
+#include <segment.h>
+
+struct segment *segment_create(void){
+    struct segment *seg = kmalloc(sizeof(struct segment));
+    seg->elf_offset = 0;
+    seg->start_vaddr = 0;
+    seg->num_pages = 0;
+}
+
+
+void segment_define(struct segment *seg, off_t elf_offset, vaddr_t start_vaddr, size_t num_pages){
+    seg->elf_offset = elf_offset;
+    seg->start_vaddr = start_vaddr;
+    seg->num_pages = num_pages;
+}
