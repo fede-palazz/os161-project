e08333b7802e8408d3119ef14169a023c6f2c768
angelodusba <angelodusba@gmail.com>
2025-01-02 16:19:07 +0100

    pt moved in addrspace

--- home/os161/src/kern/include/addrspace.h
+++ home/os161/src/kern/include/addrspace.h
@@ -65,7 +65,7 @@ struct addrspace {
                 struct segment *s_text;
                 struct segment *s_data;
                 struct segment *s_stack;
-
+                struct pt_entry *as_ptable;
         #endif
 #endif
 };
